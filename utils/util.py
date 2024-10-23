68ad5e94ee85d172fa4f44cffc83c2b3fd2435f6
Anduin1109 <53133285+Anduin1109@users.noreply.github.com>
2024-10-23 21:48:18 +0800

    first commit -- basic framework (not finished)
    Signed-off-by: Anduin1109 <53133285+Anduin1109@users.noreply.github.com>

--- utils/util.py
+++ utils/util.py
@@ -11,3 +11,7 @@ def set_seed(seed: int = 0):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+def launch_tensorboard():
+    import os
+    os.system('tensorboard --logdir=logs --port=6006')
